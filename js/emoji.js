81302be7fa1219e5065a5bcd546b1bb546d9a9f2
akkayin <ak_harold@outlook.com>
2018-10-13 20:37:47 +0800

    Site updated: 2018-10-13 20:37:46

---
+++ js/emoji.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         鼠标点击冒泡
+// @namespace    https://djzhao.js.org
+// @version      0.1
+// @description  一个用JS写的鼠标左击特效
+// @description  一些Emoji 😀😃😄😁😆😅😂🤣☺😊😚😙😗😘😍😌😉🙃🙂😇😋😜😝😛🤑🤗🤓😎🤡🤠😖😣☹🙁😕😟😔😞😒😏😫😩😤😠😡😶😐😑😯😦😥😢😨😱😳😵😲😮😧🤤😭😪😴🙄🤔😬🤥🤐💩👺👹👿😈🤕🤒😷🤧🤢👻💀☠👽👾🤖🎃😺😸😹🙏👏🙌👐😾😿🙀😽😼😻
+// @author       一碗单炒饭
+// @include      /[a-zA-z]+://[^\s]*/
+// @run-at       document_start
+// ==/UserScript==
+onload = function() {
+    var click_cnt = 0;
+    var $html = document.getElementsByTagName("html")[0];
+    var $body = document.getElementsByTagName("body")[0];
+    $html.onclick = function(e) {
+        var $elem = document.createElement("b");
+        $elem.style.color = "#E94F06";
+        $elem.style.zIndex = 9999;
+        $elem.style.position = "absolute";
+        $elem.style.select = "none";
+        var x = e.pageX;
+        var y = e.pageY;
+        $elem.style.left = (x - 10) + "px";
+        $elem.style.top = (y - 20) + "px";
+        clearInterval(anim);
+        switch (++click_cnt) {
+            case 10:
+                $elem.innerText = "😀";
+                break;
+            case 20:
+                $elem.innerText = "(๑•́ ∀ •̀๑)";
+                break;
+            case 30:
+                $elem.innerText = "😝";
+                break;
+            case 40:
+                $elem.innerText = "(๑•̀_•́๑)";
+                break;
+            case 50:
+                $elem.innerText = "（￣へ￣）";
+                break;
+            case 60:
+                $elem.innerText = "😂";
+                break;
+            case 70:
+                $elem.innerText = "૮( ᵒ̌皿ᵒ̌ )ა";
+                break;
+            case 80:
+                $elem.innerText = "😁";
+                break;
+            case 90:
+                $elem.innerText = "🤔";
+                break;
+            case 100:
+            case 101:
+            case 102:
+            case 103:
+            case 104:
+            case 105:
+                $elem.innerText = "(ꐦ°᷄д°᷅)";
+                break;
+            default:
+		// 手动更换下面这行双引号里面的内容 如"😀"
+                $elem.innerText = "😘";
+                break;
+        }
+        $elem.style.fontSize = Math.random() * 10 + 8 + "px";
+        var increase = 0;
+        var anim;
+        setTimeout(function() {
+        	anim = setInterval(function() {
+	            if (++increase == 150) {
+	                clearInterval(anim);
+			$body.removeChild($elem);
+	            }
+	            $elem.style.top = y - 20 - increase + "px";
+	            $elem.style.opacity = (150 - increase) / 120;
+	        }, 8);
+        }, 70);
+        $body.appendChild($elem);
+    };
+};
